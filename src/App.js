aea63022e8c4dd35af4e6710995a0fd5d2f95ffc
Richard Wang <richardchacha@hotmail.com>
2020-04-17 23:58:42 -0700

    update project.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import logo from './logo.svg';
 import './App.css';
 
 import Navbar from './components/Navbar';
