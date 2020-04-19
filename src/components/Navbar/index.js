6f175504a7e9d7350a8748c56ff470a97e824e3d
Richard Wang <richardchacha@hotmail.com>
2020-04-19 15:16:34 -0700

    revise links

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-sm  sticky-top navbar-dark bg-dark" role="navigation">
-      <a className="navbar-brand" href="/">Richard Wang</a>
+      <a className="navbar-brand" href="/react-portfolio">Richard Wang</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -11,9 +11,9 @@ const Navbar = () => {
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <div className="navbar-nav ml-auto" role="navigation">
-          <a className="nav-item nav-link" href="/">Home</a>
-          <a className="nav-item nav-link" href="/portfolio">Portfolio</a>
-          <a className="nav-item nav-link" href="/resume">Resume</a>
+          <a className="nav-item nav-link" href="/react-portfolio">Home</a>
+          <a className="nav-item nav-link" href="/react-portfolio/portfolio">Portfolio</a>
+          <a className="nav-item nav-link" href="/react-portfolio/resume">Resume</a>
 
         </div>
       </div>
