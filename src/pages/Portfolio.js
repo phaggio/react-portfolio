aeb94f2f0d6bb14a7c26e233fc123a479ef557a6
Richard Wang <richardchacha@hotmail.com>
2020-04-19 12:33:21 -0700

    update portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import PortfolioContainer from '../components/PortfolioContainer';
+import { Col, Container, Row } from '../components/Grid';
+import ProjectCard from '../components/ProjectCard';
+import projects from '../json/projects.json';
 
 const Portfolio = () => {
- return <PortfolioContainer />
+  //  return <PortfolioContainer />
+
+  return (
+
+    <Container>
+      <Row>
+        {projects.map(project =>
+          <Col size="sm-12 md-6 lg-4" key={project.name}>
+            <ProjectCard
+              name={project.name}
+              subtitle={project.subtitle}
+              description={project.description}
+              imgURL={project.imgURL}
+              github={project.github}
+              link={project.link}
+            />
+          </Col>
+        )}
+
+      </Row>
+    </Container>
+  )
 }
-  
+
 export default Portfolio;
