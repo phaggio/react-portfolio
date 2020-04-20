1a7647c20e69abfa43c66da68cc042bb9279bf88
Richard Wang <richardchacha@hotmail.com>
2020-04-19 18:42:52 -0700

    Updates

--- precache-manifest.6d4472cb99212c2bec6552012c3b223e.js
+++ precache-manifest.6d4472cb99212c2bec6552012c3b223e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bffaf734becbec906a8f1b9297a4a5a0",
+    "revision": "e66a206e7edbf77244346b54439891aa",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "9f7b43ccf56db66af84c",
+    "revision": "977ee83082adbeb3f3d3",
     "url": "/react-portfolio/static/css/main.5f0ef19b.chunk.css"
   },
   {
-    "revision": "b7cb0526463cd482cf71",
-    "url": "/react-portfolio/static/js/2.9fe7f1de.chunk.js"
+    "revision": "b521a2a42a1f8552b26e",
+    "url": "/react-portfolio/static/js/2.1ac13227.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/react-portfolio/static/js/2.9fe7f1de.chunk.js.LICENSE.txt"
+    "url": "/react-portfolio/static/js/2.1ac13227.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9f7b43ccf56db66af84c",
-    "url": "/react-portfolio/static/js/main.74c0df45.chunk.js"
+    "revision": "977ee83082adbeb3f3d3",
+    "url": "/react-portfolio/static/js/main.b6dd17f5.chunk.js"
   },
   {
     "revision": "c01136dc6b7e393f307b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.e7c45f8d7323d9105ca4ce4cd35d1bab.js"
+  "/react-portfolio/precache-manifest.6d4472cb99212c2bec6552012c3b223e.js"
 );
 
 self.addEventListener('message', (event) => {
