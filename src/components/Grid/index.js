ca2c5941aaba434c647b64c6fbdfcc490b160006
phaggio <richardchacha@hotmail.com>
2020-07-10 23:20:17 -0700

    update bootstrap 4.5 in index

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,35 +1,78 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Container = ({ fluid, children }) => {
+const Container = ({ fluid, other, children }) => {
   return (
-    <div className={`container${fluid ? `-fluid` : ``}`}>{children}</div>
+    <div
+      className={
+        `container${fluid ? `-fluid` : ``} 
+        ${other ? other : ``}`
+      }
+    >
+      {children}
+    </div>
   );
 }
 
 Container.propTypes = {
-  children: PropTypes.node,
-  fluid: PropTypes.bool
+  fluid: PropTypes.bool,
+  other: PropTypes.string,
+  children: PropTypes.node
 };
 
-const Row = ({ fluid, children }) => {
+
+const Row = ({ fluid, rowCols, other, children }) => {
   return (
-    <div className={`row${fluid ? `-fluid` : ``}`}>{children}</div>
+    <div className={
+      `row${fluid ? `-fluid` : ``}
+      ${rowCols ?
+        rowCols
+          .split(` `)
+          .map(size => `row-cols-${size}`)
+          .join(` `)
+        :
+        ``}
+      ${other ? other : ``}
+      `
+    }
+    >
+      {children}
+    </div>
   );
 }
 
 Row.propTypes = {
-  children: PropTypes.node,
-  fluid: PropTypes.bool
+  fluid: PropTypes.bool,
+  rowCols: PropTypes.string,
+  other: PropTypes.string,
+  children: PropTypes.node
 };
 
-const Col = ({ size, children }) => {
+const Col = ({ size, px, mx, py, my, other, children }) => {
   return (
     <div
-      className={size
-        .split(` `)
-        .map(s => `col-${s}`)
-        .join(` `)
+      className={
+        `${size
+          .split(` `)
+          .map(size => `col-${size}`)
+          .join(` `)}
+        ${px
+          .split(` `)
+          .map(px => `px-${px}`)
+          .join(` `)}
+        ${mx
+          .split(` `)
+          .map(mx => `mx-${mx}`)
+          .join(` `)}
+        ${py
+          .split(` `)
+          .map(py => `py-${py}`)
+          .join(` `)}
+        ${my
+          .split(` `)
+          .map(my => `my-${my}`)
+          .join(` `)}
+        ${other ? other : ``}`
       }
     >
       {children}
@@ -38,8 +81,13 @@ const Col = ({ size, children }) => {
 }
 
 Col.propTypes = {
-  children: PropTypes.node,
-  size: PropTypes.string
+  size: PropTypes.string,
+  px: PropTypes.string,
+  mx: PropTypes.string,
+  py: PropTypes.string,
+  my: PropTypes.string,
+  other: PropTypes.string,
+  children: PropTypes.node
 };
 
 export {
