77aa06c5e7793150054ba25e8595f62c06bc631a
phaggio <richardchacha@hotmail.com>
2020-07-12 00:25:21 -0700

    correct iFrame

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import { Container, Col, Row } from '../components/Grid';
 import resume from '../json/resume.json'
+
+const style = {
+  height: "80vh",
+  width: "100%"
+}
 
 const Resume = () => {
 
   return (
-    <iframe
-      className=""
-      src={resume[0].link}
-      width="100%"
-      height="800px"
-      title="Richard Wang"
-    />
+    <div className="mt-2">
+      <Container other="vh-75">
+        <div className="embed-responsive embed-responsive-4by3 z-depth-1-half" style={style}>
+          <iframe
+            className="embed-responsive-item"
+            src={resume[0].link}
+            title="Richard Wang"
+          />
+        </div>
 
+        <a className="btn btn-primary mt-1" href={resume[0].download}>Download</a>
+
+      </Container>
+    </div>
   )
 }
 
